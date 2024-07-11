1bd7cf27e81ee04aa88c6f3ded60083a4d328e08
Taz Hinkle <thinkle@utah.gov>
2024-07-11 14:27:18 -0600

    Populated fields in RacModal

--- src/components/RacModal.vue
+++ src/components/RacModal.vue
@@ -11,6 +11,12 @@ const newRac = ref(JSON.parse(JSON.stringify(props.rac)));
 // include Booleans to turn on/off required aspect and to show/hide elements
 const includePregnancy = ref(false);
 const includeIncome = ref(false)
+const includeAssistance = ref(false)
+const includeCopayExemptDetails = ref(false)
+const includePatientLiability = ref(false)
+const includeSpenddown = ref(false)
+const includeSPM = ref(false)
+const includeMedicareDualEligibility = ref(false)
 const emit = defineEmits(['submit', 'close']);
 const submit = () => {
 	emit('submit', newRac);
@@ -29,6 +35,30 @@ const close = () => {
 			<label>
 				<span>Include Income</span>
 				<input type="checkbox" v-model="includeIncome" />
+			</label>
+			<label>
+				<span>Include MemberIdsForAssistance</span>
+				<input type="checkbox" v-model="includeAssistance" />
+			</label>
+			<label>
+				<span>Include CopayExempt</span>
+				<input type="checkbox" v-model="includeCopayExemptDetails" />
+			</label>
+			<label>
+				<span>Include PatientLiability</span>
+				<input type="checkbox" v-model="includePatientLiability" />
+			</label>
+			<label>
+				<span>Include Spenddown</span>
+				<input type="checkbox" v-model="includeSpenddown" />
+			</label>
+			<label>
+				<span>Include SPM</span>
+				<input type="checkbox" v-model="includeSPM" />
+			</label>
+			<label>
+				<span>Include includeMedicareDualEligibility</span>
+				<input type="checkbox" v-model="includeMedicareDualEligibility" />
 			</label>
 		</div>
 		<form @submit.prevent="submit">
@@ -143,9 +173,214 @@ const close = () => {
 					/>
 				</label>
 			</div>
-			<div v-if="includeSpenddown"></div>
-			<div v-if="includeSpm"></div>
-			<div v-if="includeMedicareDualEligibility"></div>
+			<div v-if="includeSpenddown">
+				<label>
+					<span>SpenddownIndicator</span>
+					<input
+						type="text"
+						v-model="newRac.Spenddown.Information.SpenddownIndicator"
+						required
+					/>
+				</label>
+				<label>
+					<span>MetDate</span>
+					<input
+						type="text"
+						v-model="newRac.Spenddown.Information.MetDate"
+					/>
+				</label>
+				<label>
+					<span>Amount</span>
+					<input
+						type="text"
+						v-model="newRac.Spenddown.Information.Amount"
+						required
+					/>
+				</label>
+				<label>
+					<span>BillId</span>
+					<input
+						type="text"
+						v-model="newRac.Spenddown.SpenddownBills.BillDetails.BillId"
+						required
+					/>
+				</label>
+				<label>
+					<span>BillAccountNumber</span>
+					<input
+						type="text"
+						v-model="newRac.Spenddown.SpenddownBills.BillDetails.BillAccountNumber"
+						required
+					/>
+				</label>
+				<label>
+					<span>BillStartDate</span>
+					<input
+						type="date"
+						v-model="newRac.Spenddown.SpenddownBills.BillDetails.BillStartDate"
+						required
+					/>
+				</label>
+				<label>
+					<span>BillEndDate</span>
+					<input
+						type="date"
+						v-model="newRac.Spenddown.SpenddownBills.BillDetails.BillEndDate"
+						required
+					/>
+				</label>
+				<label>
+					<span>ServiceType</span>
+					<input
+						type="text"
+						v-model="newRac.Spenddown.SpenddownBills.BillDetails.ServiceType"
+						required
+					/>
+				</label>
+				<label>
+					<span>PrescriptionNumber</span>
+					<input
+						type="text"
+						v-model="newRac.Spenddown.SpenddownBills.BillDetails.PrescriptionNumber"
+					/>
+				</label>
+				<label>
+					<span>ErepCurrentUsedAmount</span>
+					<input
+						type="text"
+						v-model="newRac.Spenddown.SpenddownBills.BillDetails.ErepCurrentUsedAmount"
+						required
+					/>
+				</label>
+				<label>
+					<span>TotalBill</span>
+					<input
+						type="text"
+						v-model="newRac.Spenddown.SpenddownBills.BillDetails.TotalBill"
+						required
+					/>
+				</label>
+				<label>
+					<span>BillingProviderName</span>
+					<input
+						type="text"
+						v-model="newRac.Spenddown.SpenddownBills.BillDetails.BillingProviderName"
+						required
+					/>
+				</label>
+				<label>
+					<span>BillingProviderStreet1</span>
+					<input
+						type="text"
+						v-model="newRac.Spenddown.SpenddownBills.BillDetails.BillingProviderStreet1"
+						required
+					/>
+				</label>
+				<label>
+					<span>BillingProviderStreet2</span>
+					<input
+						type="text"
+						v-model="newRac.Spenddown.SpenddownBills.BillDetails.BillingProviderStreet2"
+						required
+					/>
+				</label>
+				<label>
+					<span>BillingProviderStreet3</span>
+					<input
+						type="text"
+						v-model="newRac.Spenddown.SpenddownBills.BillDetails.BillingProviderStreet3"
+						required
+					/>
+				</label>
+				<label>
+					<span>BillingProviderCityName</span>
+					<input
+						type="text"
+						v-model="newRac.Spenddown.SpenddownBills.BillDetails.BillingProviderCityName"
+						required
+					/>
+				</label>
+				<label>
+					<span>BillingProviderStateCode</span>
+					<input
+						type="text"
+						v-model="newRac.Spenddown.SpenddownBills.BillDetails.BillingProviderStateCode"
+						required
+					/>
+				</label>
+				<label>
+					<span>BillingProviderZipCode</span>
+					<input
+						type="text"
+						v-model="newRac.Spenddown.SpenddownBills.BillDetails.BillingProviderZipCode"
+						required
+					/>
+				</label>
+				<label>
+					<span>BillingProviderZipCodeExtension</span>
+					<input
+						type="text"
+						v-model="newRac.Spenddown.SpenddownBills.BillDetails.BillingProviderZipCodeExtension"
+					/>
+				</label>
+				<label>
+					<span>BillingProviderCountyCode</span>
+					<input
+						type="text"
+						v-model="newRac.Spenddown.SpenddownBills.BillDetails.BillingProviderCountyCode"
+					/>
+				</label>
+				<label>
+					<span>BilledPersonSuffix</span>
+					<input
+						type="text"
+						v-model="newRac.Spenddown.SpenddownBills.BillDetails.BilledPersonSuffix"
+					/>
+				</label>
+				<label>
+					<span>BilledPersonFirstName</span>
+					<input
+						type="text"
+						v-model="newRac.Spenddown.SpenddownBills.BillDetails.BilledPersonFirstName"
+						required
+					/>
+				</label>
+				<label>
+					<span>BilledPersonMiddleName</span>
+					<input
+						type="text"
+						v-model="newRac.Spenddown.SpenddownBills.BillDetails.BilledPersonMiddleName"
+					/>
+				</label>
+				<label>
+					<span>BilledPersonLastName</span>
+					<input
+						type="text"
+						v-model="newRac.Spenddown.SpenddownBills.BillDetails.BilledPersonLastName"
+						required
+					/>
+				</label>
+			</div>
+			<div v-if="includeSpm">
+				<label>
+					<span>SPMIndicator</span>
+					<input
+						type="text"
+						v-model="newRac.SPMDetails.SPMIndicator"
+						required
+					/>
+				</label>
+			</div>
+			<div v-if="includeMedicareDualEligibility">
+				<label>
+					<span>MedicareDualEligibilityStatusCode</span>
+					<input
+						type="text"
+						v-model="newRac.MedicareDualEligibilityStatusCode.MedicareDualEligibilityStatusCode"
+						required
+					/>
+				</label>
+			</div>
 			<button type="submit">Update</button>
 			<button @click="close">Cancel</button>
 		</form>
